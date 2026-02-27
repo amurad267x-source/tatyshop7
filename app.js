c419b398dbee8f088a6f2fd996e55755f52df4d8
amurad267x-source <amurad267x@gmail.com>
2026-02-27 22:45:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,19 +8,26 @@ async function loadProducts() {
   container.innerHTML = "";
 
   products.forEach(p => {
-    const card = document.createElement("div");
-    card.className = "card";
+  const card = document.createElement("div");
+  card.className = "card";
 
-    card.innerHTML = `
-      <img src="${p.image}" alt="">
-      <h3>${p.name}</h3>
-      <p>${p.description}</p>
-      <div class="price">${p.price_ton} TON</div>
-      <button class="buy-btn">Добавить в корзину</button>
-    `;
+  card.innerHTML = `
+    <img src="${p.image}" alt="">
+    <h3>${p.name}</h3>
+    <p>${p.description}</p>
+    <div class="price">${p.price_ton} TON</div>
+    <button class="buy-btn">Добавить в корзину</button>
+  `;
 
-    container.appendChild(card);
+  card.querySelector(".buy-btn").addEventListener("click", () => {
+    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+    cart.push(p);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Добавлено в корзину");
   });
-}
+
+  container.appendChild(card);
+});
+
 
 loadProducts();
